d1355f5f3a6ecf2953b129113654a177d14b4ba4
tndangana <ndangana8@gmail.com>
2020-04-21 10:38:49 +0200

    Remove Bean in Main class

--- src/main/java/zw/co/test/covid/CovidApplication.java
+++ src/main/java/zw/co/test/covid/CovidApplication.java
@@ -1,17 +1,13 @@
 package zw.co.test.covid;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class CovidApplication {
 
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CovidApplication.class, args);
