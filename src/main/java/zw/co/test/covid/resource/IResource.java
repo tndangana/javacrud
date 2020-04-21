7e0b24a4a2cdf25c454587042514f5f43dfff5b4
tndangana <ndangana8@gmail.com>
2020-04-21 10:33:53 +0200

    Create generic resource handler

--- src/main/java/zw/co/test/covid/resource/IResource.java
+++ src/main/java/zw/co/test/covid/resource/IResource.java
@@ -1,4 +1,21 @@
 package zw.co.test.covid.resource;
 
-public interface IResource {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public interface IResource<T> {
+
+
+    ResponseEntity<T> create(@Valid @RequestBody  T t);
+    ResponseEntity<T> update(@PathVariable String id, @Valid @RequestBody T t);
+    ResponseEntity<List<T>> findAll();
+    ResponseEntity<T> findById(@PathVariable  String id);
+    ResponseEntity<?> deleteById(@PathVariable String id);
+
+
 }
