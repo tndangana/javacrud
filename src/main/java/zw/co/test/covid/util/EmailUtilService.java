80c862034cd89093d879ad823ede8cfb72965243
tndangana <ndangana8@gmail.com>
2020-04-21 10:25:07 +0200

    Create email verification method

--- src/main/java/zw/co/test/covid/util/EmailUtilService.java
+++ src/main/java/zw/co/test/covid/util/EmailUtilService.java
@@ -1,4 +1,12 @@
 package zw.co.test.covid.util;
 
+import zw.co.test.covid.model.Token;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
 public interface EmailUtilService {
+
+
+  void  verifyTokenEmail(Token token) throws MessagingException, IOException;
 }
--- src/main/java/zw/co/test/covid/util/EmailUtils.java
+++ src/main/java/zw/co/test/covid/util/EmailUtils.java
@@ -1,4 +1,40 @@
 package zw.co.test.covid.util;
 
-public class EmailUtils {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import zw.co.test.covid.model.Token;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+@Service
+public class EmailUtils implements EmailUtilService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    @Override
+   public void verifyTokenEmail(Token token) throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo(token.getEmail());
+
+        helper.setSubject("Token Verification");
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>Your Token is:</h1>"+token.getToken(), true);
+
+
+        javaMailSender.send(msg);
+
+    }
 }
